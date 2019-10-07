1e42ec3ad01b9e3e0c616e630d83124dd569f09f
stepoleggg <olegstefanoyv1@gmail.com>
2019-10-07 08:00:13 +0500

    tried make as servlet

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,12 +1,45 @@
 package com.example.demo;
 
+import com.example.demo.repository.FriendsRepository;
+import com.example.demo.repository.FriendshipsRepository;
+import com.example.demo.service.FriendsService;
+import com.example.demo.service.FriendshipsService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @ComponentScan
-@EnableAutoConfiguration
-public class Application {
+@Configuration
+public class Application extends SpringBootServletInitializer {
+
+    @Bean
+    public FriendsService FriendsService(FriendsRepository friendsRepository){
+        return new FriendsService(friendsRepository);
+    }
+
+    @Bean
+    public FriendshipsService FriendshipsService(FriendshipsRepository friendshipsRepository){
+        return new FriendshipsService(friendshipsRepository);
+    }
+
+    @Bean
+    public FriendsController FriendsController(){
+        return new FriendsController();
+    }
+
+    @Bean
+    public Friends Friends(){
+        return new Friends();
+    }
+
+    @Bean
+    public Friendships Friendships(){
+        return new Friendships();
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
